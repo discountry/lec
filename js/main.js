11c1ced3ae55bf3e602d548fe99a4cff199657ee
discountry <331027793@qq.com>
2018-02-05 00:28:06 +0800

    new site

--- js/main.js
+++ js/main.js
@@ -1,125 +1,85 @@
-jQuery(document).ready(function($) {
+/*
+ Copyright (c) 2017, Sven Creations. All rights reserved.
 
-  // Header fixed and Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-      $('#header').addClass('header-fixed');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-      $('#header').removeClass('header-fixed');
-    }
-  });
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
-
-  // Initiate the AOS animation library
-  AOS.init();
-
-  // Initiate superfish on nav menu
-  $('.nav-menu').superfish({
-    animation: {
-      opacity: 'show'
-    },
-    speed: 400
-  });
-
-  // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({
-      id: 'mobile-nav'
-    });
-    $mobile_nav.find('> ul').attr({
-      'class': '',
-      'id': ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this).next().toggleClass('menu-item-active');
-      $(this).nextAll('ul').eq(0).slideToggle();
-      $(this).toggleClass("fa-chevron-up fa-chevron-down");
-    });
-
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-      $('#mobile-body-overly').toggle();
-    });
-
-    $(document).click(function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-    $("#mobile-nav, #mobile-nav-toggle").hide();
+ @requires jQuery v1.7 or later (tested on 1.11.2)
+ @author Sven Creations, svencreations@gmail.com
+*/
+"use strict";
+var isFirefox = "undefined" != typeof InstallTrigger,
+  mobileVar = isMobile.any;
+$(".bg-cover").imagesLoaded({
+  background: !0
+}, function (e) {
+  TweenMax.fromTo($(".bg-cover"), .25, {
+    autoAlpha: 0
+  }, {
+    autoAlpha: 1
+  })
+}), $(window).load(function () {
+  function e() {
+    o.countdown("resume")
   }
-
-  // Smoth scroll on page hash links
-  $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
-
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if( ! $('#header').hasClass('header-fixed') ) {
-            top_space = top_space - 20;
-          }
-        }
-
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.nav-menu').length) {
-          $('.nav-menu .menu-active').removeClass('menu-active');
-          $(this).closest('li').addClass('menu-active');
-        }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('#mobile-body-overly').fadeOut();
-        }
-        return false;
-      }
-    }
+  var n = "#EC008C";
+  $("#minimos-4").length && (n = "#F8EB31"), $("#drifter").length ? initDrifter("drifter", n) : $("#particles-js").length && initStompParticles();
+  var o = $(".st-countdown");
+  o.countdown("2018/04/01", function (e) {
+    $(this).html(e.strftime("%D Days %H:%M:%S"))
+  }), $(".btn-count").hover(function () {
+    o.countdown("pause"), $(".st-countdown").html("Bid on Flippa")
+  }, function () {
+    o.countdown("resume")
   });
-
-  // Gallery - uses the magnific popup jQuery plugin
-  $('.gallery-popup').magnificPopup({
-    type: 'image',
-    removalDelay: 300,
-    mainClass: 'mfp-fade',
-    gallery: {
-      enabled: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300,
-      easing: 'ease-in-out',
-      opener: function(openerElement) {
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-      }
-    }
-  });
-
-  // custom code
-
+  var a, t, s = $(".main-content p"),
+    r = $(".main-content h3"),
+    i = $(".main-content ul"),
+    c = "20px",
+    l = new TimelineMax({
+      paused: !0,
+      delay: .05,
+      onComplete: function () {
+        i.length && TweenMax.to(i, .75, {
+          autoAlpha: 1,
+          ease: Linear.easeNone
+        })
+      },
+      onCompleteParams: []
+    });
+  isFirefox ? c = "0px" : mobileVar && (c = "2px"), o.countdown("pause"), r.length && (t = new SplitText(r, {
+    type: "words,chars",
+    wordsClass: "gpu-hack sven-word-++",
+    charsClass: "sven-char-++"
+  }), l.staggerFromTo(t.chars, .4, {
+    y: "-50%",
+    opacity: 0
+  }, {
+    y: "0%",
+    opacity: 1,
+    ease: Bounce.easeOut
+  }, .08), l.addLabel("end"), l.call(e, [])), s.length && (a = new SplitText(s, {
+    type: "words,chars",
+    wordsClass: "gpu-hack s-word sven-word-++",
+    charsClass: "s-char sven-char-++"
+  }), TweenMax.set(a.chars, {
+    force3D: !0,
+    rotationZ: "0.01deg"
+  }), $.each(a.chars, function (e, n) {
+    var o = $(n).css("color"),
+      a = "end+=" + .04 * e;
+    l.fromTo($(n), .8, {
+      opacity: 0,
+      textShadow: "0 0 " + c + " " + o,
+      scale: 1.2,
+      color: "transparent"
+    }, {
+      opacity: 1,
+      scale: 1,
+      color: o,
+      textShadow: "none",
+      ease: Linear.easeNone
+    }, a)
+  })), TweenMax.set($(".main-content"), {
+    visibility: "visible"
+  }), TweenMax.set($(".preloader"), {
+    display: "none"
+  }), l.play()
 });
